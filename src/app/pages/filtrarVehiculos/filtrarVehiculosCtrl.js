8d7a8c321e41421ee3bc0656af9048194098a038
Santiago Vasco <svascot@gmail.com>
2016-10-03 01:39:27 -0500

    implementacion de lista de vehiculo

--- src/app/pages/filtrarVehiculos/filtrarVehiculosCtrl.js
+++ src/app/pages/filtrarVehiculos/filtrarVehiculosCtrl.js
@@ -7,7 +7,64 @@
   /** @ngInject */
   function FiltrarVehiculosCtrl($scope,$rootScope,VehiculosService) {
 
-    $scope.vehiculos = {};
+    $scope.vehiculos = [
+      {
+        "AgenciumId":"2",
+        "placa":"tpt028",
+        "capacidad":"15",
+        "modelo":"2017",
+        "audio":"false",
+        "video":"true",
+        "aire":"true",
+        "bano":"false",
+        "reclinable":"true",
+        "imagen":"123456",
+        "marca":"Toyota",
+        "referencia":"Tacoma"
+      },
+      {
+        "AgenciumId":"2",
+        "placa":"tpt029",
+        "capacidad":"15",
+        "modelo":"2017",
+        "audio":"false",
+        "video":"true",
+        "aire":"flase",
+        "bano":"true",
+        "reclinable":"true",
+        "imagen":"123456",
+        "marca":"Toyota",
+        "referencia":"Tundra"
+      },
+      {
+        "AgenciumId":"2",
+        "placa":"tpt030",
+        "capacidad":"15",
+        "modelo":"2017",
+        "audio":"false",
+        "video":"true",
+        "aire":"true",
+        "bano":"true",
+        "reclinable":"true",
+        "imagen":"123456",
+        "marca":"Kia",
+        "referencia":"Sorento"
+      },
+      {
+        "AgenciumId":"2",
+        "placa":"tpt031",
+        "capacidad":"15",
+        "modelo":"2017",
+        "audio":"false",
+        "video":"true",
+        "aire":"true",
+        "bano":"true",
+        "reclinable":"true",
+        "imagen":"123456",
+        "marca":"Chevrolet",
+        "referencia":"Tahoe"
+      },
+    ];
 
     $scope.filtrar = function(filtro){
       console.dir(filtro)
