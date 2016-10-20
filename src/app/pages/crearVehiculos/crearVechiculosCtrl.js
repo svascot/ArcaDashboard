c4d3fac621984a366bc87aaa6e7a9307e5396999
jmejiaz <juan.mejia.zuluaga@gmail.com>
2016-10-19 23:20:03 -0500

    editando foto

--- src/app/pages/crearVehiculos/crearVechiculosCtrl.js
+++ src/app/pages/crearVehiculos/crearVechiculosCtrl.js
@@ -30,10 +30,25 @@
 
     $scope.actualizarVehiculo= function(vehiculo){
       if (confirm("Desea guardar los cambios?") == true) {
-        VehiculosService.actualizarVehiculo(vehiculo).then(function(vehiculoActualizado){
+        if(vehiculo.nuevaImagen){
+          
+          uploadToAWS.uploadFiles(new Array(vehiculo.nuevaImagen)).then(function(urls){
+            vehiculo.imagen= urls[0].endPoint          
+            VehiculosService.actualizarVehiculo(vehiculo).then(function(vehiculoActualizado){
+            vehiculo = vehiculoActualizado;
+            openedToasts.push(toastr["success"]("Vehiculo actualizado", "Exito", $rootScope.toastDefautlOptions));
+          })
+         })
+
+        }
+        else{
+          VehiculosService.actualizarVehiculo(vehiculo).then(function(vehiculoActualizado){
           vehiculo = vehiculoActualizado;
           openedToasts.push(toastr["success"]("Vehiculo actualizado", "Exito", $rootScope.toastDefautlOptions));
         })
+        }
+
+        
 
       }
     }
