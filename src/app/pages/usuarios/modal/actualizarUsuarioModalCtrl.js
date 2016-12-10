36e938e5c82b0874e349db9597cb63e7d11a4e63
jmejiaz <juan.mejia.zuluaga@gmail.com>
2016-12-09 20:54:21 -0500

    imagen necesaria para actualizar usuario ya no requerida

--- src/app/pages/usuarios/modal/actualizarUsuarioModalCtrl.js
+++ src/app/pages/usuarios/modal/actualizarUsuarioModalCtrl.js
@@ -17,25 +17,47 @@
 
     $scope.actualizarUsuario = function(usuario){
 
-        uploadToAWS.uploadFiles(new Array(usuario.imagen)).then(function(urls){
+      if(usuario.nuevaImagen){
+         uploadToAWS.uploadFiles(new Array(usuario.imagen)).then(function(urls){
         usuario.foto= urls[0].endPoint
         UsuariosService.actualizarUsuario(usuario).then(function(response){
           openedToasts.push(toastr["success"]("Usuario actualizado", "Exito", $rootScope.toastDefautlOptions));
           $scope.usuarios.push(response.data);
              $timeout(function() {
                            $scope.$apply()
+                            
             });
                 console.dir($scope.usuarios)
             },function(err){
               console.dir(err);
                 if(err.status == 412){
                   openedToasts.push(toastr["error"](err.message, "No tienens permiso para actualizar ese tipo de usuario", $rootScope.toastDefautlOptions));
-
+                  
                 }
             })
         })
+      }
+      else{
+        UsuariosService.actualizarUsuario(usuario).then(function(response){
+          openedToasts.push(toastr["success"]("Usuario actualizado", "Exito", $rootScope.toastDefautlOptions));
+          $scope.usuarios.push(response.data);
+             $timeout(function() {
+                           $scope.$apply()
+                           
+            });
+                console.dir($scope.usuarios)
+            },function(err){
+              console.dir(err);
+                if(err.status == 412){
+                  openedToasts.push(toastr["error"](err.message, "No tienens permiso para actualizar ese tipo de usuario", $rootScope.toastDefautlOptions));
+                  
+                }
+            })        
+      }
+       
 
       $rootScope.currentOpenModal.close();
+     
     }
 
 
